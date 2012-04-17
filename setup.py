bb8cffdf1868a92c06a78d3e07ae2ed153b92c03
Jason Harvey <alienth@reddit.com>
2012-04-17 13:28:07 -0700

    custom version bump

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 # or saved.  Do not modify them directly here.
 # NB: PACKAGES is deprecated
 NAME = "ZenPacks.chudler.xmppBot"
-VERSION = "2.0~alienth1"
+VERSION = "2.0~alienth2"
 AUTHOR = "Colin Hudler"
 LICENSE = ""
 NAMESPACE_PACKAGES = ['ZenPacks', 'ZenPacks.chudler']
